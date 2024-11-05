2bda10465284845c14c881b30053d65658a18f8b
Peter Williams <retex.williams@gmail.com>
2024-11-05 23:18:02 +0000

    removed unused useRef

--- src/components/TheSessionTuneDetailsPage/TuneSettingsList.jsx
+++ src/components/TheSessionTuneDetailsPage/TuneSettingsList.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Box, Typography, Card, CardContent, Stack } from "@mui/material";
 import abcjs from "abcjs";
 import TuneAudioPlayer from "../TuneAudioPlayer";
